5924e7d7276f1847b69e8a55412898c7b3e7f9e7
Boris Vera <boris@wearevera.com>
2025-01-20 00:09:55 +0000

    Use HTTPS import-docs

--- scripts/import-docs.sh
+++ scripts/import-docs.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 # Configuración
-REPO="git@github.com:veraandcompany/project-docs-poc.git"
-BRANCH="main"
+REPO="veraandcompany/project-docs-poc" # Cambia al nombre del repo que quieres clonar
 FOLDER="docs"
 TARGET="imported-docs/tms-docs"
 
 # Crear carpeta temporal
 TMP_DIR=$(mktemp -d)
 
-# Clonar el repositorio
+# Clonar el repositorio utilizando HTTPS y GITHUB_TOKEN
 echo "Cloning repository..."
-git clone --depth 1 --branch "$BRANCH" "$REPO" "$TMP_DIR"
+git clone --depth 1 --branch main https://x-access-token:${GITHUB_TOKEN}@github.com/${REPO}.git "$TMP_DIR"
 
 # Mover la carpeta específica a su destino final
 echo "Moving $FOLDER to $TARGET..."
-mkdir -p "$(dirname "$TARGET")"
 rm -rf "$TARGET"
 mv "$TMP_DIR/$FOLDER" "$TARGET"
 
